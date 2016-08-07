6cd4f70a3d27b74e5ddcb65debf9d033785af0f1
Suhail <suhailj17@gmail.com>
2016-08-06 22:22:04 -0400

    imported circlething

--- app/src/main/java/com/firstblock/mobile/moody/MainActivity.java
+++ app/src/main/java/com/firstblock/mobile/moody/MainActivity.java
@@ -3,11 +3,25 @@ package com.firstblock.mobile.moody;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import me.angrybyte.circularslider.CircularSlider;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CircularSlider circular = (CircularSlider) findViewById(R.id.circular);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.layout);
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+
+
+
     }
+
+
+
 }
